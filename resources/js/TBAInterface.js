2858078f880c8921587aceb8e032400c833f19c8
857roboscout <857roboscouting@gmail.com>
2025-02-22 13:38:02 -0600

    changed apicode

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "WZrwSECqnvn8loE6pg8AQrdkwBliVkeVMKlBBGFNG3O5OeV1pIF3sxPhrceM36zL";
 /**
  * Get list of teams in event
  *
